f814ef41c97b39ce6200ff49ac00229d552fc8ec
Buddhisha Wijerathne <147919313+Cpox123@users.noreply.github.com>
2026-08-11 21:11:17 +0530

    Update 2_Bulk_Prediction.py

--- pages/2_Bulk_Prediction.py
+++ pages/2_Bulk_Prediction.py
@@ -1,4 +1,4 @@
-```python
+
 import streamlit as st
 import pandas as pd
 
@@ -200,4 +200,4 @@ if uploaded_file is not None:
                     file_name="sentiment_predictions.csv",
                     mime="text/csv",
                 )
-```
+
